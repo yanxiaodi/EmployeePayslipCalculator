68df02f9e24e04c75dbfa4e4d0714b85ba5253ea
Yan Xiaodi <yan_xiaodi@hotmail.com>
2017-10-17 13:09:35 +0800

    Add models.

---
+++ EmployeePayslipCalculator/EmployeePayslipCalculator.Models/EmployeeInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeePayslipCalculator.Models
+{
+    public class EmployeeInfo
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int AnnualSalary { get; set; }
+        public decimal SuperRate { get; set; }
+    }
+}
---
+++ EmployeePayslipCalculator/EmployeePayslipCalculator.Models/PayslipInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayslipCalculator.Models
+{
+    public class PayslipInfo
+    {
+        public int Id { get; set; }
+        public string EmployeeId { get; set; }
+        public virtual EmployeeInfo Employee { get; set; }
+        public DateTime PayPeriod { get; set; }
+        public decimal GrossIncome { get; set; }
+        public decimal IncomeTax { get; set; }
+        public decimal NetIncome { get; set; }
+        public decimal Super { get; set; }
+    }
+}
