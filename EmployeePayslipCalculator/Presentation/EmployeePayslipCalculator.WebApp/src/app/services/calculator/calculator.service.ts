1fa7f46ee2649f3f3eddf004ed71ad0898ea512c
Yan Xiaodi <yan_xiaodi@hotmail.com>
2017-10-18 14:09:57 +0800

    modify some files.

--- EmployeePayslipCalculator/Presentation/EmployeePayslipCalculator.WebApp/src/app/services/calculator/calculator.service.ts
+++ EmployeePayslipCalculator/Presentation/EmployeePayslipCalculator.WebApp/src/app/services/calculator/calculator.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { GlobalContextService, HttpClientService } from '../index';
 
 @Injectable()
 export class CalculatorService {
 
-  constructor() { }
+  constructor(public globalContext: GlobalContextService, public httpClient: HttpClientService) { }
+
+  public async getGateList() {
+    const url = `${this.globalContext.server}GateMonitorApi/GetGateList`;
+    return await this.httpClient.get<Array<TGateInfo>>(url);
+  }
 
 }
